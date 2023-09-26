03df1cad3e0c5d8155d19c44e715e1b4c6b67352
ninfe08802 <ninfe08802@localhost.localdomain>
2023-09-26 23:29:38 +0200

    Añadimos modificaciones en hostscan

--- hostscan.sh
+++ hostscan.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function ctrl_c(){
-
+	echo -e "\n\n Salimos..\n"
 	exit 1 # matamos el proceso
 }
 
 #Ctrl+C
-trap ctrl_SIGINT
+trap ctrl_c SIGINT
 
 for i in $(seq 1 254); do
 	timeout 1 bash -c "ping -c 1 192.168.0.$i" &>/dev/null && echo "El host 192.168.0.$i esta levantado - ACTIVO" &
